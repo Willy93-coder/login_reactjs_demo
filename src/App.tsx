9c13195f9b056ec353f08608b6cb77fd443d6151
Guillermo Cifre <guillecg13@gmail.com>
2024-12-11 12:15:13 +0100

    Add scaffold

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="grid h-lvh items-center justify-items-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
