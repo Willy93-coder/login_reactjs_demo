6d60e989ffd44e1e4c60a89d5c57ce6c94a1009b
Guillermo Cifre <guillecg13@gmail.com>
2024-12-30 21:18:35 +0100

    Added signUpWithEmailAndPassword function

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,14 +1,26 @@
+import { SignUpInputsModel } from "@/components/sign-up-form/model";
 import { mapToUserModel, User } from "@/lib/models/user-model";
 import { supabase } from "@/supabase/supabase-client";
 
-async function signInWithGoogleProvider() {
+async function signInWithGoogleProvider(): Promise<void> {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
   });
   if (error) throw new Error("Login error");
 }
 
-async function signOut() {
+async function signUpWithEmailAndPassword({
+  email,
+  password,
+}: SignUpInputsModel): Promise<void> {
+  const { error } = await supabase.auth.signUp({
+    email: email,
+    password: password,
+  });
+  if (error) throw new Error("Sign up error");
+}
+
+async function signOut(): Promise<void> {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error("Sign out error");
 }
@@ -21,4 +33,9 @@ async function getUserInfo(): Promise<User | undefined> {
   return mapToUserModel(user);
 }
 
-export { signInWithGoogleProvider, signOut, getUserInfo };
+export {
+  signInWithGoogleProvider,
+  signUpWithEmailAndPassword,
+  signOut,
+  getUserInfo,
+};
