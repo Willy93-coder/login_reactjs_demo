83ec12b7227207d0cb440fd4f87d44fd477da34e
Guillermo Cifre <guillecg13@gmail.com>
2024-12-31 19:58:31 +0100

    Added toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { Route, Routes } from "react-router";
 import { Login } from "./pages/login";
 import { Home } from "./pages/home";
 import { CreateAccount } from "./pages/create-account";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
-    <div className="grid h-lvh items-center justify-items-center">
+    <div className="flex h-lvh items-center justify-center">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/home" element={<Home />} />
       </Routes>
+      <Toaster />
     </div>
   );
 }
