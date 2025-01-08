9769fc5f0d15f2599c0cd9c5c1f58adbc7e3dc5e
Guillermo Cifre <guillecg13@gmail.com>
2025-01-08 20:42:51 +0100

    Added VITE_REDIRECT_URL_RESET_PASSWORD environment variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_KEY: string;
   readonly VITE_SUPABASE_URL: string;
+  readonly VITE_REDIRECT_URL_RESET_PASSWORD: string;
 }
 
 interface ImportMeta {
